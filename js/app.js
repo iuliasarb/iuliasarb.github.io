2ab3996276388c600923ff9b6fd17c1555a16076
IS <is@ISs-MacBook-Pro.local>
2018-05-24 22:17:12 +0200

    added skills

--- js/app.js
+++ js/app.js
@@ -43,4 +43,41 @@ $(function() {
     }
   });
 
+
+  // ===== show year ===== 
+  var date = new Date();
+  var yr = date.getFullYear();
+  $('#year').text(yr);
+
 });
+
+  // ===== animation start ===== 
+
+
+    var contain = document.querySelector('.bg-3');
+    var bar1 = document.querySelector('.bar1');
+    bar1.firstChild.nextSibling.innerHTML = bar1.dataset.percent;
+    var bar2 = document.querySelector('.bar2');
+    bar2.firstChild.nextSibling.innerHTML = bar2.dataset.percent;
+    var bar3 = document.querySelector('.bar3');
+    bar3.firstChild.nextSibling.innerHTML = bar3.dataset.percent;
+    var bar4 = document.querySelector('.bar4');
+    bar4.firstChild.nextSibling.innerHTML = bar4.dataset.percent;
+    var bar5 = document.querySelector('.bar5');
+    bar5.firstChild.nextSibling.innerHTML = bar5.dataset.percent;
+    var bar6 = document.querySelector('.bar6');
+    bar6.firstChild.nextSibling.innerHTML = bar6.dataset.percent;
+
+    contain.onmouseenter = function(){
+      bar1.style['animationPlayState'] = 'inherit';
+      bar2.style['animationPlayState'] = 'inherit';
+      bar3.style['animationPlayState'] = 'inherit';
+      bar4.style['animationPlayState'] = 'inherit';
+      bar5.style['animationPlayState'] = 'inherit';
+      bar6.style['animationPlayState'] = 'inherit';
+    };
+
+
+
+
+
